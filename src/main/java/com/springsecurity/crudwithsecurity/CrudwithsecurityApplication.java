3a49182c432f3cea5e849d51ee216001108e30bd
Gabriel <gabriel_martinhoq@hotmail.com>
2025-05-20 16:25:38 -0300

    Added files and structure of the project

---
+++ src/main/java/com/springsecurity/crudwithsecurity/CrudwithsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.crudwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudwithsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudwithsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/crudwithsecurity/CrudwithsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.crudwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudwithsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
